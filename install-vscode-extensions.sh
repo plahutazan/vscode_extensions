392327f6b54b4434546485f4f42e168057cd99c3
Zan <plahutazan3@gmail.com>
2025-08-30 00:45:27 +0200

    Removed icon comments in the script

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 🦀 Rust Extensions
+# Rust Extensions
 code --install-extension rust-lang.rust-analyzer
 code --install-extension swellaby.vscode-rust-test-adapter
 code --install-extension panicbit.cargo
@@ -10,7 +10,7 @@ code --install-extension vadimcn.vscode-lldb
 code --install-extension aaron-bond.better-comments
 code --install-extension rust-lang.rust
 
-# 📦 General Programming Extensions
+# General Programming Extensions
 code --install-extension eamodio.gitlens
 code --install-extension christian-kohler.path-intellisense
 code --install-extension BracketPairColorDLW.bracket-pair-color-dlw
@@ -22,14 +22,14 @@ code --install-extension alefragnani.bookmarks
 code --install-extension IBM.output-colorizer
 code --install-extension usernamehw.errorlens
 
-# ⚡ Embedded / no_std / ESP Extensions
+# Embedded / no_std / ESP Extensions
 code --install-extension marus25.cortex-debug
 code --install-extension ms-vscode.hexeditor
 code --install-extension ms-vscode.vscode-serial-monitor
 code --install-extension ms-vscode.vscode-embedded-tools
 code --install-extension benoitf.memory-viewer
 
-# 📝 Config / TOML / JSON / YAML
+# Config / TOML / JSON / YAML
 code --install-extension tamasfe.even-better-toml
 code --install-extension redhat.vscode-yaml
 code --install-extension eriklynd.json-tools
